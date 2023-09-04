b17dc58b8e4dfb61752eafc2f0b71879105b1870
Cynthiamor <kabita262011@hotmail.com>
2023-09-04 14:51:11 -0500

    tratando

---
+++ more-movie/src/index.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+
+// eslint-disable-next-line react/no-deprecated
+ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+/*
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+*/
